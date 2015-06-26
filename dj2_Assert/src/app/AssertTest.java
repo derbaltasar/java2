551afb999d27523d4f06d99add9f5949bb9b106e
Student <Student@10.101.102.90>
2015-06-26 13:25:02 +0200

    assert

---
+++ dj2_Assert/src/app/AssertTest.java
@@ -0,0 +1,17 @@
+package app;
+
+public class AssertTest {
+
+	
+	
+	public static void m1(int a){
+		assert(a!=0);
+		
+		System.out.println("kkkk");
+		
+	}
+	
+	public static void main(String[] args) {
+		m1(0);
+	}
+}
